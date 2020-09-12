c515a521f70062c71f173c1ba07e3d940e63a347
imranspahic <62642796+imranspahic@users.noreply.github.com>
2020-09-12 21:18:57 +0200

    facebook login, stream auth changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:task3/screens/secondScreen.dart';
+
 import './screens/first_screen.dart';
 // import './screens/secondScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import './functions/login_functions.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,10 +17,13 @@ void main() async{
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: FirstScreen(),
+    return StreamProvider.value(
+        value: LoginFunction().user,
+          child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: LoginFunction().user ==null ? FirstScreen() : SecondScreen(),
+      ),
     );
   }
 }
