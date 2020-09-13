3f2839f5232992ae88242c7a41a4a17243067c72
NedimKaravdic <nkaravdic10@gmail.com>
2020-09-13 03:15:02 +0100

    Procitati NAPOMENA!!! file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-
 import './screens/first_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import './functions/login_functions.dart';
+import 'screens/Wrapper.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
@@ -16,14 +16,13 @@ void main() async{
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return StreamProvider<User>.value(
-        value: LoginFunction().user,
-          child: MaterialApp(
+      value: LoginFunction().user,
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
         // home: LoginFunction().user ==null ? FirstScreen() : SecondScreen(),
-        home: FirstScreen(),
+        home: Wrapper(),
       ),
     );
   }
